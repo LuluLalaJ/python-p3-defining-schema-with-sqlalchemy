dd33234daa79189b686c7e7c93233d5f41522778
Chen <chen.jiang.1206@gmail.com>
2023-05-11 09:58:36 -0400

    practice done

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,113 @@
 #!/usr/bin/env python3
+from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import (create_engine, desc,
+    Index, Column, DateTime, Integer, String, func)
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
 
 if __name__ == '__main__':
-    pass
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
+
+    # use our engine to configure a 'Session' class
+    Session = sessionmaker(bind=engine)
+    # use 'Session' class to create 'session' object
+    session = Session()
+
+    albert_einstein = Student(
+        name="Albert Einstein",
+        email="albert.einstein@zurich.edu",
+        grade=6,
+        birthday=datetime(
+            year=1879,
+            month=3,
+            day=14
+        ),
+    )
+
+    alan_turing = Student(
+        name="Alan Turing",
+        email="alan.turing@sherborne.edu",
+        grade=11,
+        birthday=datetime(
+            year=1912,
+            month=6,
+            day=23
+        ),
+    )
+
+    session.bulk_save_objects([albert_einstein, alan_turing])
+    session.commit()
+
+    # students = session.query(Student)
+    # print([student for student in students])
+    # print(f"New student ID is {albert_einstein.id}.")
+    # print(f"New student ID is {alan_turing.id}.")
+
+    students = session.query(Student).all()
+    print("students", students)
+
+    names = session.query(Student.name).all()
+    print("names", names)
+
+    students_by_name = session.query(Student.name).order_by(Student.name).all()
+    print('students by name', students_by_name)
+
+    # oldest_student = session.query(
+    #     Student.name, Student.birthday).order_by(
+    #     Student.birthday).limit(1).all()
+
+    oldest_student = session.query(
+        Student.name, Student.birthday).order_by(
+        Student.birthday).first()
+
+    print(oldest_student)
+
+    student_count = session.query(func.count(Student.id)).first()
+    print('student count', student_count)
+
+    query = session.query(
+        Student).filter(
+        Student.name.like('%Alan%'), Student.grade == 11).all()
+
+    for record in query:
+        print(record.name)
+
+    # for student in session.query(Student):
+    #     student.grade += 1
+
+    # session.commit()
+
+    session.query(Student).update({
+        Student.grade: Student.grade + 1
+    })
+    print([(student.name, student.grade) for student in session.query(Student)])
+
+
+    query = session.query(Student).filter(Student.name == "Albert Einstein")
+
+    # ab = query.first()
+    # session.delete(ab)
+    # session.commit()
+    query.delete()
+
+    ab = query.first()
+    print(ab)
